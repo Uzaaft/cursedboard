69468b5e5ec94403f0a3a1b384abfebd8efb0d76
Uzair Aftab <uzaaft@outlook.com>
2025-04-17 19:07:56 +0200

    feat: working prototype of sending clipboard contents over to linux side

--- macos/src/main.rs
+++ macos/src/main.rs
@@ -1,7 +1,7 @@
 mod error;
 mod config;
 
-use std::{ops::Deref, thread, time::Duration};
+use std::{io::Write, net::TcpStream, ops::Deref, thread, time::Duration};
 
 use objc2::rc::Retained;
 use objc2_app_kit::{NSPasteboard, NSPasteboardTypeString};
@@ -27,14 +27,26 @@ impl Deref for Clipboard{
 }
 
 fn main() -> Result<(), error::AppError > {
+    let mut stream = TcpStream::connect("dev.bobr:34254")?;
+
     let cp = Clipboard::default();
     let default_interval = Duration::from_millis(500); // TODO: Config value
-
+    let mut prev_count = 0;
     loop {
-        thread::sleep(default_interval);
         let changecount = unsafe{ cp.changeCount()};
+        // If changecount is not changed, skip
+        if changecount == prev_count{
+            thread::sleep(default_interval);
+            continue;
+        }
         let text = unsafe { cp.stringForType(NSPasteboardTypeString) };
-        println!("Changecount: {changecount}, Contents: {text:?}");
-    }
+        if let Some(s) = &text {
+            stream.write_all(&s.to_string().into_bytes())?;
+        }
 
+        prev_count = changecount;
+        println!("Changecount: {prev_count}, Contents: {:?}", &text);
+        // Sleep
+        thread::sleep(default_interval);
+    }
 }
