ee9727ad74fbd68fe63f8efea7d47b9b83c3c409
Uzair Aftab <uzaaft@outlook.com>
2025-04-18 16:05:28 +0200

    wip: linux socket

--- linux/src/main.zig
+++ linux/src/main.zig
@@ -1,51 +1,31 @@
 const std = @import("std");
 const net = std.net;
-const posix = std.posix;
+const print = std.debug.print;
 
 pub fn main() !void {
-    const address = try std.net.Address.parseIp("0.0.0.0", 34254);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    const tpe: u32 = posix.SOCK.STREAM;
-    const protocol = posix.IPPROTO.TCP;
-    const listener = try posix.socket(address.any.family, tpe, protocol);
-    defer posix.close(listener);
+    const loopback = try net.Ip4Address.parse("0.0.0.0", 34255);
+    const localhost = net.Address{ .in = loopback };
+    var server = try localhost.listen(.{
+        .reuse_address = true,
+    });
+    defer server.deinit();
 
-    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
-    try posix.bind(listener, &address.any, address.getOsSockLen());
-    try posix.listen(listener, 128);
-    var buf: [1024]u8 = undefined;
-    var client_address: net.Address = undefined;
-    var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+    const addr = server.listen_address;
+    print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
 
-    const socket = try posix.accept(listener, &client_address.any, &client_address_len, 0);
-    defer posix.close(socket);
+    var client = try server.accept();
+    defer client.stream.close();
 
-    std.debug.print("{} connected\n", .{client_address});
-    while (true) {
+    print("Connection received! {} is sending data.\n", .{client.address});
+    const messageLength: [8]u8 = undefined;
 
-        const read = posix.read(socket, &buf) catch |err| {
-            std.debug.print("error reading: {}\n", .{err});
-            continue;
-        };
-        std.debug.print("{s}", .{buf});
-        // Clear buffer
-        // For now we are just printing this out. The goal is to pass the contents of the buffer to wayland clipboard
-        buf = undefined;
+    // Program flow:
+    // First 8 bytes contains length of the message.
+    // The rest of the bytes contains the message, with the afforementioned length
+    const length = try client.stream.reader().read(messageLength);
 
-        if (read == 0) {
-            continue;
-        }
-
-    }
-}
-
-fn write(socket: posix.socket_t, msg: []const u8) !void {
-    var pos: usize = 0;
-    while (pos < msg.len) {
-        const written = try posix.write(socket, msg[pos..]);
-        if (written == 0) {
-            return error.Closed;
-        }
-        pos += written;
-    }
+    print("{} says message is: {s} bytes long\n", .{ client.address, length });
 }
